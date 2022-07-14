d972174b6ac43a8e2df1c73da4cec3465f701199
toqa-yaqout <toqayaqout31@outlook.com>
2022-07-14 23:27:47 +0200

    make product list root route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProductListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
