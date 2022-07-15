99a534efa31451cd88f7d0aa1ad6b14ccbf5506e
toqa-yaqout <toqayaqout31@outlook.com>
2022-07-15 11:14:15 +0200

    show list of products and refactor style

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
+import { Product } from 'src/models/product';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
+  products: Product[] = [];
 
-  constructor() { }
+  constructor(private productService: ProductService) {}
 
   ngOnInit() {
+    this.productService.getProducts().subscribe(data => {
+      this.products = data;
+    });
   }
-
 }
